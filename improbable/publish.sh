9435bc1ff2377a9d918226104878451a939e4a02
Sean Robertson <serobertson@gmail.com>
2020-05-27 00:15:10 +0100

    Add publish pipeline and rewrite the publish step to use the docker container

--- improbable/publish.sh
+++ improbable/publish.sh
@@ -1,32 +1,27 @@
 #!/usr/bin/env bash
+###
+# Builds and pushes Prow containers to the Improbable GCR
+###
 
-###
-# Builds Hook and pushes the container to the Improbable GCR
-###
+set -e -o pipefail
+
+# cd to the directory where this bash script is located at.
+cd "$(dirname "$0")"
+repo_root=$(dirname "$(pwd -P)")
 
 export PROW_REPO_OVERRIDE="eu.gcr.io/windy-oxide-102215"
 export DOCKER_REPO_OVERRIDE="${PROW_REPO_OVERRIDE}"
 export EDGE_PROW_REPO_OVERRIDE="${PROW_REPO_OVERRIDE}"
 
-# By default, try to use Bazelisk, since this repo doesn't have `tools/bazel` set up.
-# If that fails, try just `bazel`, and if that fails, just bail out.
-BAZEL_BIN="bazelisk"
-command -v "${BAZEL_BIN}" >"/dev/null"
-if [[ $? != 0 ]]; then
-  BAZEL_BIN="bazel"
-  echo "WARNING: Bazelisk not found, attempting to use 'bazel' directly."
-  echo "Without bazelisk installed, you may need to manually install the correct Bazel version."
-  command -v "${BAZEL_BIN}" >"/dev/null"
-  if [[ $? != 0 ]]; then
-    echo "Bazel not found - install it to build this tool."
-    exit 1
-  fi
-fi
-
-echo "Using ${BAZEL_BIN} as Bazel."
-
-"${BAZEL_BIN}" \
-  --bazelrc="$(dirname "$0")/bazelrc" \
-  run \
-  --config=imp-release \
-  //improbable:improbable-push
+docker build -t dockerized_tests dockerized_tests
+docker run -e LOCAL_USER_ID="$(id -u)" \
+  -v "${repo_root}":/repo:rw \
+  --workdir=/repo \
+  --entrypoint=/usr/local/bin/entrypoint.sh \
+  -it \
+  dockerized_tests \
+  bash -c 'bazel \
+    --bazelrc="/repo/improbable/bazelrc" \
+    run \
+    --config=imp-release \
+    //improbable:improbable-push'
