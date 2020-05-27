4ce59732056aa64b0188d5492a28b339f3b35741
Sean Robertson <serobertson@gmail.com>
2020-05-27 07:32:57 +0100

    set up google credentials in containers

--- improbable/publish.sh
+++ improbable/publish.sh
@@ -9,6 +9,16 @@ set -e -o pipefail
 cd "$(dirname "$0")"
 repo_root=$(dirname "$(pwd -P)")
 
+GCLOUD_CONFIG_VOLUME_ARGS=()
+
+if [[ -r "${HOME}/.config/gcloud/application_default_credentials.json" ]]; then
+  GCLOUD_CONFIG_VOLUME_ARGS=(
+    "--volume=${HOME}/.config/gcloud:/gcloud_config"
+    "-e=CLOUDSDK_CONFIG=/gcloud_config"
+    "-e=GOOGLE_APPLICATION_CREDENTIALS=/gcloud_config/application_default_credentials.json"
+  )
+fi
+
 export PROW_REPO_OVERRIDE="eu.gcr.io/windy-oxide-102215"
 export DOCKER_REPO_OVERRIDE="${PROW_REPO_OVERRIDE}"
 export EDGE_PROW_REPO_OVERRIDE="${PROW_REPO_OVERRIDE}"
@@ -16,12 +26,13 @@ export EDGE_PROW_REPO_OVERRIDE="${PROW_REPO_OVERRIDE}"
 docker build -t dockerized_tests dockerized_tests
 docker run -e LOCAL_USER_ID="$(id -u)" \
   -v "${repo_root}":/repo:rw \
+  "${GCLOUD_CONFIG_VOLUME_ARGS[@]}" \
   --workdir=/repo \
   --entrypoint=/usr/local/bin/entrypoint.sh \
   -it \
   dockerized_tests \
   bash -c 'bazel \
-    --bazelrc="/repo/improbable/bazelrc" \
+    --bazelrc="improbable/bazelrc" \
     run \
     --config=imp-release \
     //improbable:improbable-push'
--- improbable/run_dockerized_tests.sh
+++ improbable/run_dockerized_tests.sh
@@ -5,15 +5,26 @@ set -e -o pipefail
 cd "$(dirname "$0")"
 repo_root=$(dirname "$(pwd -P)")
 
+GCLOUD_CONFIG_VOLUME_ARGS=()
+
+if [[ -r "${HOME}/.config/gcloud/application_default_credentials.json" ]]; then
+  GCLOUD_CONFIG_VOLUME_ARGS=(
+    "--volume=${HOME}/.config/gcloud:/gcloud_config"
+    "-e=CLOUDSDK_CONFIG=/gcloud_config"
+    "-e=GOOGLE_APPLICATION_CREDENTIALS=/gcloud_config/application_default_credentials.json"
+  )
+fi
+
 docker build -t dockerized_tests dockerized_tests
 docker run -e LOCAL_USER_ID="$(id -u)" \
   -v "${repo_root}":/repo:rw \
+  "${GCLOUD_CONFIG_VOLUME_ARGS[@]}" \
   --workdir=/repo \
   --entrypoint=/usr/local/bin/entrypoint.sh \
   -it \
   dockerized_tests \
   bash -c 'bazel \
-      --bazelrc="/repo/improbable/bazelrc" \
+      --bazelrc="improbable/bazelrc" \
       test \
       --config=imp-ci \
       //prow/... \
